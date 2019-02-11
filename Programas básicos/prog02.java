6ecdb550f3c5b7950322f49955c5898cce70a890
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-02-11 18:35:35 -0200

    Create prog02.java

---
+++ Programas básicos/prog02.java
@@ -0,0 +1,35 @@
+package programatestejava;
+
+import java.util.Scanner;
+import java.lang.String;
+
+public class ProgramaTesteJava {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a,b,maior,menor;
+        
+        System.out.print("Digite um numero: ");
+        a = entrada.nextInt();
+        System.out.print("Digite outro numero: ");
+        b = entrada.nextInt();
+        
+        if(a>b){
+            maior = a;
+            menor = b;
+        }
+        else{
+            maior = b;
+            menor = a;
+        }
+        if(maior != menor){
+            while(menor+1 < maior){
+                System.out.printf("%d ",menor+1);
+                menor++;
+            }
+            System.out.print("\n");
+        }
+        else{
+            System.out.println("Números iguais!");
+        }
+    }
+}
