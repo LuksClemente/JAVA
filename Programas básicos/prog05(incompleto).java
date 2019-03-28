a05bb83ee7dc7e35b45f5c4942506221a79d9155
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-03-28 20:08:22 -0300

    Create prog05(incompleto).java

---
+++ Programas básicos/prog05(incompleto).java
@@ -0,0 +1,52 @@
+import java.lang.String;
+
+class Automovel{
+	public String modelo;
+	public int ano;
+	private boolean ligado;
+	
+	public Automovel(String m, int a){
+		modelo = m;
+		ano = a;
+		ligado = false;
+	}
+	
+	public void liga(){
+		ligado = true;
+	}
+	
+	public void desliga(){
+		ligado = false;
+	}
+}
+
+class Motorista{
+	private String nome;
+	private Automovel carro;
+	
+	public Motorista(String n, Automovel a){
+		nome = n;
+		carro = a;
+	}
+	
+	public String obterNome(){
+		return nome;
+	}
+	
+	public Automovel obterCarro(){
+		return carro;
+	}
+}
+class MeuPrograma{
+	private Automovel carro1;
+	private Motorista motorista1, motorista2;
+	
+	public static void main(String args[]){
+		carro1 = new Automovel("Chevette", 87);
+		motorista1= new Motorista("Joao", carro1);
+		motorista2 = new Motorista("Pedro", carro1);
+		
+		System.out.println(motorista1.obterNome());
+		System.out.println(motorista2.obterNome());
+	}
+}
