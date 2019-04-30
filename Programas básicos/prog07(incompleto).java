0e8522bbbd48fef364ca63b1a3c0778150ebf124
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-04-30 20:15:47 -0300

    Update prog07(incompleto).java

--- Programas básicos/prog07(incompleto).java
+++ Programas básicos/prog07(incompleto).java
@@ -2,43 +2,42 @@ import java.io.*;
 import java.lang.Math;
 
 class Calculadora{
-	public float r,a,b,l1,l2,l3,area;
-	
 	private static float Calcula(float r){
-		area = r*r*3.14159265359;
+		float area = (Math.pow(r,2))*3.14159265359;
 		return area;
 	}
 	private static float Calcula(float b, float a){
-		area = b*a;
+		float area = b*a;
 		return area;
 	}
 	private static float Calcula(float l1,float l2, float l3){
 		float p = (l1+l2+l3)/2;
-		area = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
+		float area = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
 		return area;
 	}
 }
 public class Areas{
 	public static void main(String[] args){
+		Calculadora c = new Calculadora();
 		float r,a,b,l1,l2,l3,area;
-		if(args.lenght == 1){
+		if(args.length == 1){
 			r = Float.parseFloat(args[0]);
-			area = Calcula(r);
+			area = c.Calcula(r);
 			System.out.printf("Area do circulo: %f\n", area);
 		}
-		else if(args.lenght == 2){
+		else if(args.length == 2){
 			a = Float.parseFloat(args[0]);
 			b = Float.parseFloat(args[1]);
-			area = Calcula(b, a);
+			area = c.Calcula(b, a);
 			System.out.printf("Area do retangulo: %f\n", area);
 		}
-		else if(args.lenght == 3){
+		else if(args.length == 3){
 			l1 = Float.parseFloat(args[0]);
 			l2 = Float.parseFloat(args[1]);
 			l3 = Float.parseFloat(args[2]);
-			area = Calcula(l1, l2, l3);
+			area = c.Calcula(l1, l2, l3);
 			System.out.print("Area do triangulo: %f\n", area);
-			if(l1 == l2 == l3){
+			if((l1 == l2)&&(l2 == l3)){
 				System.out.println("O triangulo eh equilatero!");
 			}
 			else if((l1 == l2)||(l2 == l3)||(l3 == l1)){
